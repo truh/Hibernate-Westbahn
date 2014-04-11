9c0bf39ef56a65492d7ddf2de6383b7553b5561f
Jakob Klepp <jklepp@student.tgm.ac.at>
2014-04-11 13:47:38 +0200

    POJO => Beans
    Bahnhof
    Benutzer
    Einzelticket
    Reservierung

--- src/westbahn/model/Einzelticket.java
+++ src/westbahn/model/Einzelticket.java
@@ -4,4 +4,11 @@ public class Einzelticket extends Ticket {
 
 	private TicketOption ticketOption;
 
+    public TicketOption getTicketOption() {
+        return ticketOption;
+    }
+
+    public void setTicketOption(TicketOption ticketOption) {
+        this.ticketOption = ticketOption;
+    }
 }
