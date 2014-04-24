ab39d764df5b3839f0c1dfc9f85420a7b3af0c9b
Andreas Willinger <awi95@gmx.at>
2014-04-24 13:04:31 +0200

    Attempt to fix stuff

--- src/westbahn/BahnhofValidator.java
+++ src/westbahn/BahnhofValidator.java
@@ -1,9 +1,17 @@
 package westbahn;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.Target;
+import java.lang.annotation.Retention;
+
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.*;
+
+@Target( { TYPE, ANNOTATION_TYPE })
+@Retention(RUNTIME)
 @Constraint(validatedBy = BahnhofValidatorImpl.class)
 @Documented
 public @interface BahnhofValidator 
